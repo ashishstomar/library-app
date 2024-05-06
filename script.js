fe3aa4372c64d3198d66abff2deee25d059ef89b
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-05-07 03:02:46 +0530

    feat- new book entry via btn

--- script.js
+++ script.js
@@ -2,30 +2,32 @@ const input = document.querySelectorAll('input');
 const submit = document.querySelector('submit');
 const form = document.querySelector('form');
 const main = document.querySelector("main");
+const removeBook = document.querySelector("button");
+const newBookBtn = document.querySelector(".newBookBtn");
+const fieldset = document.querySelector("fieldset");
 
-
-const myLibrary = [];
+const myLibrary = [
     // {
     //     title: "Let Us C",
     //     author: "Yashwant k",
-    //     pages: 430,
     //     genre: "Comp Science",
+    //     pages: 430,
     //     read: "Yes"
     // },
     // {
     //     title: "Data Structure",
     //     author: "Narasimha",
-    //     pages: 512,
     //     genre: "Engineering",
+    //     pages: 512,
     //     read: "No"
     // },
-// ];
+];
 
-function Book(title, author, pages, read) {
+function Book(title, author, genre, pages, read) {
     this.title = title;
     this.author = author;
-    this.pages = pages;
     this.genre = genre;
+    this.pages = pages;
     this.read = read;
 }
 
@@ -33,7 +35,7 @@ function addBookToLibrary() {
     let title = input[0].value;
     let author = input[1].value;
     let genre = input[2].value;
-    let pages = input[3].value;
+    let pages = parseInt(input[3].value);
     let read = input[4].value;
 
     const bookNew = new Book(title, author, genre, pages, read);
@@ -42,22 +44,47 @@ function addBookToLibrary() {
     displayBooks()
 }
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    addBookToLibrary();
-    console.log(myLibrary);
-    
-})
 
 function displayBooks() {
-    section.innerHTML = '';
-
     myLibrary.forEach((book) => {
-        let section = document.createElement('section');
-        section.innerHTML = `<h4>${book.title}</h4><p>${book.author}</p><p>${book.pages}</p></p><p>${book.genre}</p>`
-        section.Classlist.add("bookInfo");
-        main.appendChild(section);
-        
+        if(book === myLibrary[myLibrary.length - 1]) {
+            let section = document.createElement('section');
+            section.classList.add("bookInfo");
+            main.appendChild(section);
+    
+            let title = document.createElement('h4');
+            title.textContent = book.title;
+            section.appendChild(title);
+    
+            let author = document.createElement('p');
+            author.textContent = `Author: ${book.author}`;
+            section.appendChild(author);
+    
+            let genre = document.createElement('p');
+            genre.textContent = `Genre: ${book.genre}`;
+            section.appendChild(genre);
+    
+            let pages = document.createElement('p');
+            pages.textContent = `Pages: ${book.pages}`;
+            section.appendChild(pages);
+    
+            let read = document.createElement('p');
+            read.textContent = `Status: ${book.read}`;
+            section.appendChild(read);
+        }
     })
 }
 
+
+form.addEventListener("submit", (e) => {
+    
+    addBookToLibrary();
+    e.preventDefault();
+    newBookBtn.style.display = "block"
+    fieldset.style.display = "none";
+})
+
+newBookBtn.addEventListener("click", () => {
+    fieldset.style.display = "block";
+    newBookBtn.style.display = "none";
+})
