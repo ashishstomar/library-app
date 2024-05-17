aa6b373b727f58ac34953d55ca7fb25e4dd31220
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-05-17 16:53:40 +0530

    feat- read toggle & delete book card

--- script.js
+++ script.js
@@ -9,9 +9,10 @@ const closeFormBtn = document.querySelector("#closeFormBtn");
 const deleteBtn = document.querySelector(".deleteBtn");
 const readtoggle = document.querySelector("#readtoggle");
 
-const myLibrary = [];
+let myLibrary = [];
 
-function Book(title, author, genre, pages, read) {
+function Book(id, title, author, genre, pages, read) {
+    this.id = id;
     this.title = title;
     this.author = author;
     this.genre = genre;
@@ -20,78 +21,84 @@ function Book(title, author, genre, pages, read) {
 }
 
 function addBookToLibrary() {
+    let id = myLibrary.length;
     let title = input[0].value;
     let author = input[1].value;
     let genre = input[2].value;
     let pages = parseInt(input[3].value);
     let read = document.querySelector('input[name="isRead"]:checked').value;
 
-    const bookNew = new Book(title, author, genre, pages, read);
+    const bookNew = new Book(id, title, author, genre, pages, read);
     myLibrary.push(bookNew);
     displayBooks()
-    console.log(Book.id)
 }
 
 
 function displayBooks() {
+    const bookCards = document.querySelector('.bookCards');
+    bookCards.innerHTML = '';
     myLibrary.forEach(book => {
-        if(book === myLibrary[myLibrary.length - 1]) {
-            let section = document.createElement('section');
-            section.classList.add("bookInfo");
-            main.appendChild(section);
-            
-            let divMenuContainer = document.createElement('div');
-            section.appendChild(divMenuContainer);
+        let bookInfo = document.createElement('article');
+        bookInfo.classList.add("bookInfo");
+        bookCards.appendChild(bookInfo);
+        
+        let divMenuContainer = document.createElement('div');
+        bookInfo.appendChild(divMenuContainer);
 
-            let icon = document.createElement('i');
-            divMenuContainer.appendChild(icon);
-            icon.classList.add('threeDots');
-            icon.addEventListener('click', () => {
-                ul.style.display = ul.style.display === 'block'? 'none' : 'block';
-            })
+        let icon = document.createElement('i');
+        divMenuContainer.appendChild(icon);
+        icon.classList.add('threeDots');
+        icon.addEventListener('click', () => {
+            ul.style.display = ul.style.display === 'block'? 'none' : 'block';
+        })
 
-            let ul = document.createElement('ul');
-            divMenuContainer.appendChild(ul);
-            ul.classList.add('threeDotsOptions');
+        let ul = document.createElement('ul');
+        divMenuContainer.appendChild(ul);
+        ul.classList.add('threeDotsOptions');
     
-            let liRead = document.createElement('li')
-            ul.appendChild(liRead);
-            let readBtn = document.createElement('button')
-            liRead.appendChild(readBtn);
-            readBtn.classList.add('readBtn');
-            readBtn.textContent = 'Read';
-            
-            let liDelete = document.createElement('li');
-            ul.appendChild(liDelete);
-            let deleteBtn = document.createElement('button');
-            liDelete.appendChild(deleteBtn);
-            deleteBtn.classList.add('deleteBtn');
-            deleteBtn.textContent = 'Delete';
-            deleteBtn.addEventListener('click', () => {
-                deleteBook()
-            })
-
-
-            let title = document.createElement('h4');
-            title.textContent = book.title;
-            section.appendChild(title);
-    
-            let author = document.createElement('p');
-            author.textContent = `Author: ${book.author}`;
-            section.appendChild(author);
-    
-            let genre = document.createElement('p');
-            genre.textContent = `Genre: ${book.genre}`;
-            section.appendChild(genre);
-    
-            let pages = document.createElement('p');
-            pages.textContent = `Pages: ${book.pages}`;
-            section.appendChild(pages);
-    
-            let read = document.createElement('p');
-            read.textContent = `Status: ${book.read}`;
-            section.appendChild(read);
+        let liRead = document.createElement('li')
+        ul.appendChild(liRead);
+        let readBtn = document.createElement('button')
+        liRead.appendChild(readBtn);
+        readBtn.classList.add('readBtn');
+        readBtn.textContent = book.read === 'Unread' ? 'Read' : 'Unread';
+        readBtn.onclick = () => {
+            book.read = toggleRead(book.read);
+            console.log(readBtn.textContent);
+            displayBooks();
         }
+        
+        let liDelete = document.createElement('li');
+        ul.appendChild(liDelete);
+        let deleteBtn = document.createElement('button');
+        liDelete.appendChild(deleteBtn);
+        deleteBtn.classList.add('deleteBtn');
+        deleteBtn.textContent = 'Delete';
+        deleteBtn.onclick = () => {
+            deleteBook(book.id)
+        }
+        
+
+        let title = document.createElement('h4');
+        title.textContent = book.title;
+        bookInfo.appendChild(title);
+    
+        let author = document.createElement('p');
+        author.textContent = `Author: ${book.author}`;
+        bookInfo.appendChild(author);
+    
+        let genre = document.createElement('p');
+        genre.textContent = `Genre: ${book.genre}`;
+        bookInfo.appendChild(genre);
+    
+        let pages = document.createElement('p');
+        pages.textContent = `Pages: ${book.pages}`;
+        bookInfo.appendChild(pages);
+    
+        let read = document.createElement('p');
+        read.textContent = `Status: ${book.read}`;
+        bookInfo.appendChild(read);
+        
     })
 }
 
@@ -111,10 +118,16 @@ closeFormBtn.addEventListener("click", () => {
     newBookBtn.style.display = "block";
 })
 
-function deleteBook() {
-    main.addEventListener('click', (e) => {
-        if(e.target.classList === 'deleteBtn') {
-            console.log(e.target.classList)
-        }
-    })
+function deleteBook(id) {
+    myLibrary = myLibrary.filter(book => book.id !== id);;
+    displayBooks();
+}
+
+function toggleRead(isRead) {
+    if(isRead === 'Unread') {
+       return 'Read';
+    }
+    else {
+        return 'Unread'
+    }
 }
